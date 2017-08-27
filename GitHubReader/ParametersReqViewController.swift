7ca0e9a65b9afd19c1457bf472fdf959e1446467
Wojciech Pelc <wojciech.pelc@vp.pl>
2017-08-27 07:47:31 +0200

    nowy Controller url parameters request

---
+++ GitHubReader/ParametersReqViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ParametersReqViewController.swift
+//  GitHubReader
+//
+//  Created by Wojciech Pelc on 27/08/2017.
+//  Copyright © 2017 Wojciech Pelc. All rights reserved.
+//
+
+import UIKit
+
+class ParametersReqViewController: UIViewController {
+    
+//    @IBOutlet weak var gitHubUsername: UITextField!
+//    @IBOutlet weak var pageSize: UITextField!
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        self.gitHubUsername.text = AppSettings.shared.githubUser
+//        self.pageSize.text = String(describing: AppSettings.shared.pageSize) //"\(AppSettings.shared.pageSize)"
+//    }
+//    
+//    @IBAction func cancel(_ sender: UIBarButtonItem) {
+//        self.dismiss(animated: true, completion: nil)
+//    }
+//    @IBAction func save(_ sender: UIBarButtonItem) {
+//        AppSettings.shared.githubUser = self.gitHubUsername.text ?? ""
+//        if let value = Int(self.pageSize.text ?? "20") {
+//            AppSettings.shared.pageSize = value
+//        }
+//        AppSettings.shared.save()
+//        self.dismiss(animated: true, completion: nil)
+//    }
+}
